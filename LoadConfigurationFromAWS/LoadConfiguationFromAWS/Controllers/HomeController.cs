3c8ff899f098313e9be2f92a7f7662e9a8a4ca44
Lamond Lu <lunan@shintechchina.com>
2019-03-12 09:41:51 +0800

    Initial check-in

---
+++ LoadConfigurationFromAWS/LoadConfiguationFromAWS/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LoadConfiguationFromAWS.Models;
+using Microsoft.Extensions.Options;
+
+namespace LoadConfiguationFromAWS.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly Settings _settings;
+
+        public HomeController(IOptions<Settings> settings)
+        {
+            _settings = settings.Value;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["configval"] = _settings.Config1;
+            ViewData["configval2"] = _settings.Config2;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ LoadConfigurationFromAWS/LoadConfiguationFromAWS/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoadConfiguationFromAWS
+{
+    public class Settings
+    {
+        public string Config1 { get; set; }
+
+        public string Config2 { get; set; }
+    }
+}
