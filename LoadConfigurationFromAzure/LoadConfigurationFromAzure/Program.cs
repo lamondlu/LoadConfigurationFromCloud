08eac70c4ed8bb65512afc6868c740c728da8214
Lamond Lu <lunan@shintechchina.com>
2019-03-12 11:45:09 +0800

    add code for Azure App Configuration

---
+++ LoadConfigurationFromAzure/LoadConfigurationFromAzure/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using Microsoft.Extensions.Logging;
+
+namespace LoadConfigurationFromAzure
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    var settings = config.Build();
+                    config.AddAzureAppConfiguration("xxx");
+                })
+                .UseStartup<Startup>();
+    }
+}
---
+++ LoadConfigurationFromAzure/LoadConfigurationFromAzure/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoadConfigurationFromAzure
+{
+    public class Settings
+    {
+        public string Config1 { get; set; }
+
+        public string Config2 { get; set; }
+    }
+}
